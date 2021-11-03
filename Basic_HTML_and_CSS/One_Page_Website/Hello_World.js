c7f307d4ec0b9b08ca1bf702bba046cc09699a96
Cesarintapia <93293727+Cesarintapia@users.noreply.github.com>
2021-11-03 15:57:24 -0700

    First CSS animation

---
+++ Basic_HTML_and_CSS/One_Page_Website/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = "Hello World!";
+console.log (greeting);
+
+var greeting_parrots = 'Welcom to Node parrots.js'
+console.log(greeting_parrots)
+
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentence1);
